498992500d378f5053779fc3652a59dee9f75ffd
HermesBotBr <contatohermesbot@gmail.com>
2025-03-11 19:17:38 -0300

    Update save-subscription.js

--- api/save-subscription.js
+++ api/save-subscription.js
@@ -8,7 +8,11 @@ export default async function handler(req, res) {
       const subscriptionText = JSON.stringify(subscription);
       
       // Envia a subscription para o endpoint remoto que salva no subscriptions.txt
-      const response = await fetch("https://f7a0be410680.ngrok.app/subscription", {
+      
+      const response = await fetch(getNgrokUrl('subscriptions.txt'));;
+
+
+        const response = await fetch(getNgrokUrl('subscriptions.txt'), {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: subscriptionText })
