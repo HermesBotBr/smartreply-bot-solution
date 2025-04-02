636299ffd0cd941eb21a3c13e563c820ffb0beae
HermesBotBr <contatohermesbot@gmail.com>
2025-04-02 10:05:46 -0300

    Update force-refresh-pack.js

--- api/force-refresh-pack.js
+++ api/force-refresh-pack.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-import { addToUpdateQueue } from '../lib/update-queue';
+import { addToUpdateQueue } from '../lib/update-queue.js';
 
 
 export default async function handler(req, res) {
